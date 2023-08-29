1ae6ca4a4ae63b4cc04e2d7f649c6ab28c906ab4
Juan Esteban <cardonaospinajuanesteban@gmail.com>
2023-08-28 22:00:18 -0500

    micr1

--- mserv1.py
+++ mserv1.py
@@ -25,9 +25,9 @@ class ArchivoServicer(archivo_pb2_grpc.ArchivoServicer):
 def main():
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     archivo_pb2_grpc.add_ArchivoServicer_to_server(ArchivoServicer(), server)
-    server.add_insecure_port('[::]:{MICSERVER1_PORT}')
+    server.add_insecure_port(f'[::]:{MICSERVER1_PORT}')
     server.start()
-    print("Microservicio mserv1 escuchando en el {MICSERVER1_PORT}...")
+    print(f"Microservicio mserv1 escuchando en el {MICSERVER1_PORT}...")
     server.wait_for_termination()
 
 if __name__ == '__main__':
